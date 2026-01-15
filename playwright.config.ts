fd508a59982d2ccc868b23694808f6f43a4fb958
sitanshuparida11 <sitanshuparida@gmail.com>
2026-01-16 00:01:28 +0530

    Fix Esm

--- playwright.config.ts
+++ playwright.config.ts
@@ -2,17 +2,17 @@ import { defineConfig } from '@playwright/test';
 
 export default defineConfig({
   testDir: './tests',
-  timeout: 30 * 1000,
+  use: {
+    headless: true,
+    screenshot: 'only-on-failure',
+    video: 'retain-on-failure',
+    trace: 'on-first-retry',
+  },
 
   retries: process.env.CI ? 2 : 0,
   workers: process.env.CI ? 2 : undefined,
 
   reporter: [['html', { open: 'never' }]],
 
-  use: {
-    headless: true,
-    screenshot: 'only-on-failure',
-    video: 'retain-on-failure',
-    trace: 'on-first-retry'
-  }
+ 
 });
