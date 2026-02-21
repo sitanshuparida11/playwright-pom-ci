ed36820aad2c3d9eef0b8443d0383cb36c36394f
sitanshuparida11 <sitanshuparida@gmail.com>
2026-02-21 18:07:09 +0530

    headless as true

--- playwright.config.ts
+++ playwright.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from '@playwright/test';
 export default defineConfig({
   testDir: './tests',
   use: {
-    headless: false,
+    headless: true,
     screenshot: 'only-on-failure',
     video: 'retain-on-failure',
     trace: 'on-first-retry',
